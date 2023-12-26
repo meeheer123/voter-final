a2e0ed1f9ca7ce06fccc24d999ad493f6b7141d7
= <=>
2023-12-26 15:59:49 +0530

    form almost compelte

--- app.py
+++ app.py
@@ -2,32 +2,31 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-cities = ['Nagpur', 'City2']  # Add more cities as needed
+cities = ['Nagpur', 'City2']
 regions = {'Nagpur': ['East', 'West', 'Northeast'], 'City2': ['Region1', 'Region2']}
 wards = {'East': ['Wardhaman Nagar', 'Another Ward'], 'West': ['Ward1', 'Ward2'],
          'Northeast': ['Ward3', 'Ward4'], 'Region1': ['WardA', 'WardB'], 'Region2': ['WardX', 'WardY']}
 
 @app.route('/')
 def index():
-    return render_template('index_flask.html', cities=cities)
+    return render_template('index.html', cities=cities)
+
+@app.route('/get_regions/<city>', methods=['GET'])
+def get_regions(city):
+    return jsonify(regions.get(city, []))
+
+@app.route('/get_wards/<region>', methods=['GET'])
+def get_wards(region):
+    return jsonify(wards.get(region, []))
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
-    data = request.get_json()
+    data = request.json
+    print('Received data:', data)
+    # Add your logic to process the data here
 
-    # Assuming the data received contains city, region, and ward information
-    selected_city = data.get('dropdown1', '')
-    selected_region = data.get('dropdown2', '')
-    selected_ward = data.get('dropdown3', '')
-
-    # Process the received data as needed
-    print('Selected City:', selected_city)
-    print('Selected Region:', selected_region)
-    print('Selected Ward:', selected_ward)
-
-    # You can perform further processing or validation here
-
-    return jsonify({'message': 'Form data received successfully'})
+    # For demonstration purposes, let's return a simple response
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
     app.run(debug=True)
